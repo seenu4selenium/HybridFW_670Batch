da6a014223638e8d16343552684782f7e7942331
“seenu4selenium” <seenu4selenium@gmail.com>
2025-05-12 20:17:47 -0600

    First Commit

---
+++ src/test/java/com/objectrepository/Locators.java
@@ -0,0 +1,13 @@
+package com.objectrepository;
+
+import org.openqa.selenium.By;
+
+public class Locators {
+	
+	//Here QA will store all application Locators
+	//public final By pagename_ElementName_elementType = By.name("Sdgffdgd");
+	public final By fbLogin_ForgotPassword_Link = By.linkText("Forgot password?");
+	public final By fbLogin_Email_editBox = By.name("email");
+	
+	
+}
---
+++ src/test/java/com/testscenarios/FB_Login.java
@@ -0,0 +1,42 @@
+package com.testscenarios;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import com.objectrepository.Locators;
+
+public class FB_Login {
+
+	// Here will define the object for Locators class
+	Locators loc = new Locators();
+
+	@Test
+	public void f() throws Exception {
+
+		// Create object for PROPERTIES class from JAVA
+		Properties prop = new Properties();
+		// Give property file path to the system
+		FileInputStream fi = new FileInputStream(".\\src\\test\\resources\\testdata\\QA_Environment.properties");
+		// Load the test data to property file variable
+		prop.load(fi);
+
+		WebDriver driver;
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+
+		driver.get(prop.getProperty("FB_URL"));
+
+		Thread.sleep(2000);
+
+		driver.findElement(loc.fbLogin_ForgotPassword_Link).click();
+		Thread.sleep(2000);
+
+		driver.findElement(loc.fbLogin_Email_editBox).sendKeys(prop.getProperty("FB_UN"));
+
+	}
+}
